e429449d7d2cb0f8a9325150a8989d62571e668a
Milinda-Jayawardhana <shehanj809@gmail.com>
2024-07-25 00:17:16 +0530

    f size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,10 +14,10 @@ export default function Footer() {
               <span className='text-xl bold'>shopping</span>
               <span className='text-green-600 '>.</span>
             </h1>
-            <h2 className='pt-4 pb-2 text-2xl font-bold '>Contact</h2>
-            <h3><span className='text-[18px] '> Address :</span> <span className=' text-white/60'>No255, Matara, Sri Lanka</span></h3>
-            <h3><span className='text-[18px] '> Email :</span> <span className=' text-white/60'>shehanj809@gmail.com</span></h3>
-            <h3><span className='text-[18px] '> Phone :</span><span className=' text-white/60'>+94 71 550 8827/+94 74 069 0333</span></h3>
+            <h2 className='pt-4 pb-2 text-xl font-bold md:text-2xl '>Contact</h2>
+            <h3><span className='text-[14px]md:text-[18px] '> Address :</span> <span className=' text-white/60'>No255, Matara, Sri Lanka</span></h3>
+            <h3><span className='text-[14px] md:text-[18px] '> Email :</span> <span className=' text-white/60'>shehanj809@gmail.com</span></h3>
+            <h3><span className='text-[14px] md:text-[18px] '> Phone :</span><span className=' text-white/60'>+94 71 550 8827/+94 74 069 0333</span></h3>
             <div className='mt-4 mb-6'>
               <Socials 
                 containerStyles= 'flex gap-4'
@@ -27,26 +27,26 @@ export default function Footer() {
           </div>
 
           <div className='flex flex-col items-start justify-start pt-[70px] md:pt-0 '>
-            <h1 className='text-2xl'>About</h1>
-            <h2 className='pt-4 text-white/60'>About Us</h2>
-            <h2 className='pt-2 text-white/60 '>Delivery Information</h2>
-            <h2 className='pt-2 text-white/60'>Privacy & Policy</h2>
-            <h2 className='pt-2 text-white/60'> Terms & Conditions</h2>
-            <h2 className='pt-2 text-white/60'>Contact Us</h2>
+            <h1 className='text-xl md:text-2xl'>About</h1>
+            <h2 className='pt-4 text-white/60 text-[14px] md:text-[18px]'>About Us</h2>
+            <h2 className='pt-2 text-white/60 text-[14px] md:text-[18px] '>Delivery Information</h2>
+            <h2 className='pt-2 text-white/60 text-[14px] md:text-[18px]'>Privacy & Policy</h2>
+            <h2 className='pt-2 text-white/60 text-[14px] md:text-[18px]'> Terms & Conditions</h2>
+            <h2 className='pt-2 text-white/60 text-[14px] md:text-[18px]'>Contact Us</h2>
           </div>
 
           <div className='flex flex-col items-start justify-start '>
-            <h1 className='text-2xl'>My Account</h1>
-            <h2 className='pt-4 text-white/60'>Log In</h2>
-            <h2 className='pt-2 text-white/60'>View Cart</h2>
-            <h2 className='pt-2 text-white/60'>My Whishlist</h2>
-            <h2 className='pt-2 text-white/60'> Track My Order</h2>
-            <h2 className='pt-2 text-white/60'>Help</h2>
+            <h1 className='text-xl md:text-2xl'>My Account</h1>
+            <h2 className='pt-4 text-white/60 text-[14px] md:text-[18px]'>Log In</h2>
+            <h2 className='pt-2 text-white/60 text-[14px] md:text-[18px]'>View Cart</h2>
+            <h2 className='pt-2 text-white/60 text-[14px] md:text-[18px]'>My Whishlist</h2>
+            <h2 className='pt-2 text-white/60 text-[14px] md:text-[18px]'> Track My Order</h2>
+            <h2 className='pt-2 text-white/60 text-[14px] md:text-[18px]'>Help</h2>
           </div>
 
           <div className='flex flex-col items-start justify-start gap-2 '>
-            <h1 className='text-2xl'> Install App</h1>
-            <h2 className='pt-4 text-white/60'>From App store & Google Play</h2>
+            <h1 className='text-xl md:text-2xl'> Install App</h1>
+            <h2 className='pt-4 text-white/60 text-[14px] md:text-[18px]'>From App store & Google Play</h2>
             <div className='flex items-center gap-4 px-2 bg-gray-600 rounded-lg'>
               <FaApple className='text-3xl text-green-400' />
               <div className='flex flex-col '>
